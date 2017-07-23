3d6ebb454125b3344d850dd7549eaaffd3d61d37
SeongMinKim <sksmsdhorm@naver.com>
2017-07-24 02:43:52 +0900

    Update changeword.py

--- changeword.py
+++ changeword.py
@@ -1,13 +1,13 @@
 import os, re
 
 
-directory = os.listdir('D:/git/www git (2)')
-os.chdir('D:/git/www git (2)')
+directory = os.listdir('')
+os.chdir(' 경로입력')
 
 for file in directory:
 	open_file = open(file, 'r')
 	read_file = open_file.read()
 	regex = re.compile('ĂŁžĆžß ÇŇ łťżë') //바꿀단어
-	read_file = regex.sub('šŮ˛Ü łťżë', read_file)//
+	read_file = regex.sub('šŮ˛Ü łťżë', read_file) //바뀔단어
 	write_file = open(file, 'w')
 	write_file.write(read_file)
