c0a1e9168f0057d7de6d5e4f93bd208590e479b4
SeongMinKim <sksmsdhorm@naver.com>
2017-07-24 02:43:04 +0900

    Update changeword.py

--- changeword.py
+++ changeword.py
@@ -7,7 +7,7 @@ os.chdir('D:/git/www git (2)')
 for file in directory:
 	open_file = open(file, 'r')
 	read_file = open_file.read()
-	regex = re.compile('цё╬ф╬ъ гр Ё╩©К')
-	read_file = regex.sub('╧ы╡э Ё╩©К', read_file)
+	regex = re.compile('д┌е│е╬д├е╬ц÷ ц┤е┤ е┌е╔е╪ц╚') //К╟■Й©─К▀╗Л√╢
+	read_file = regex.sub('е║е╝к⌡ц° е┌е╔е╪ц╚', read_file)//
 	write_file = open(file, 'w')
 	write_file.write(read_file)
